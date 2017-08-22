8483151e0c53e03511f354d8c0908f37609b8db6
Murphy <shmilyclxy@163.com>
2017-08-22 18:28:54 +0800

    add: addTraveller page

--- src/view/roomDetails/addTraveller.vue
+++ src/view/roomDetails/addTraveller.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="addTraveller">
-        <h1>入住人</h1>
+        <div class="traveller-form">
+            <div class="traveller-form-item">
+                <label class="form-label" for="name">姓名</label>
+                <input class="form-input" type="text" placeholder="请确保证与证件姓名一致">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +16,27 @@ export default {
 
 <style lang="scss">
 .addTraveller {
+    .traveller-form {
+        background: #fff;
+        border-top: 1px solid #e6e6e6;
+        border-bottom: 1px solid #e6e6e6;
+        .traveller-form-item {
+            display: flex;
+            padding: 0 32px;
+            height: 76px;
+            line-height: 76px;
+            font-size: 28px;
+            .form-label {
+                width: 160px;
+                color: #333;
+            }
+            .form-input {
+                flex: 1;
+            }
+        }
+        .traveller-form-item + .traveller-form-item {
+            border-top: 1px solid #e6e6e6;
+        }
+    }
 }
 </style>
