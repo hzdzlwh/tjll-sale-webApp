659728389df873d76aee0ca26bf8c0c9a046b0f3
lxj <xinjian.lin@dingdandao.com>
2017-09-12 14:22:58 +0800

    close debug

--- src/util/wxsdk.js
+++ src/util/wxsdk.js
@@ -2,7 +2,7 @@
 * @Author: lxj
 * @Date:   2017-08-17 13:40:52
 * @Last Modified by:   lxj
-* @Last Modified time: 2017-09-12 13:37:56
+* @Last Modified time: 2017-09-12 14:22:45
 */
 import http from './http.js';
 import { getDirectWebShareDate } from './share';
@@ -46,7 +46,7 @@ export function wxShare() {
     getSign(timestamp, noncestr)
         .then(data => {
             wx.config({
-                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: appId, // 必填，公众号的唯一标识
                 timestamp, // 必填，生成签名的时间戳
                 nonceStr: noncestr, // 必填，生成签名的随机串
