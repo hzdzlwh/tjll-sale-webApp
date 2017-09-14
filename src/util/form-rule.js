53b3069051ac1e3c44671fc68f1d8b01dce13536
Murphy <shmilyclxy@163.com>
2017-09-14 16:46:20 +0800

    fix

--- src/util/form-rule.js
+++ src/util/form-rule.js
@@ -4,11 +4,11 @@ export const guestForm = (form) => {
         console.log('姓名错误');
         return '姓名错误'
     }
-    const phoneReg = /^[1][3,4,5,7,8][0-9]{9}$/
-    if (!(form.phone.length > 2 && form.phone.length < 21)) {
-        console.log('手机号错误');
-        return '手机号错误'
-    }
+    // const phoneReg = /^[1][3,4,5,7,8][0-9]{9}$/
+    // if (!(form.phone.length > 2 && form.phone.length < 21)) {
+    //     console.log('手机号错误');
+    //     return '手机号错误'
+    // }
     const idReg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/
     if (!(form.idCardNum.length > 2 && form.idCardNum.length < 21)) {
         console.log('身份证错误');
